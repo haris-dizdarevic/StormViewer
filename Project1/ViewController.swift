097fa2dd3c55286d4063339180529df0a7c1496c
Dizdarevic <Dizdarevic@fidor.de>
2017-08-26 20:34:56 +0200

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Haris Dizdarevic on 26/08/2017.
+//  Copyright © 2017 Haris Dizdarevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
